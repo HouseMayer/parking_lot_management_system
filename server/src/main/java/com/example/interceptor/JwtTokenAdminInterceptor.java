289b6fb5054219baf00bb3882f6ff664f6f32656
HouseMayer <2677296152@qq.com>
2024-02-28 23:04:48 +0800

    feat:jwt令牌校验的拦截器

--- server/src/main/java/com/example/interceptor/JwtTokenAdminInterceptor.java
+++ server/src/main/java/com/example/interceptor/JwtTokenAdminInterceptor.java
@@ -1,63 +1,77 @@
-//package com.example.interceptor;
-//
-//import com.example.constant.JwtClaimsConstant;
-//import com.example.context.BaseContext;
-//import com.example.properties.JwtProperties;
-//import com.example.utils.JwtUtil;
-//import io.jsonwebtoken.Claims;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.method.HandlerMethod;
-//import org.springframework.web.servlet.HandlerInterceptor;
-//
-//
-///**
-// * jwt令牌校验的拦截器
-// */
-//@Component
-//@Slf4j
-//public class JwtTokenAdminInterceptor implements HandlerInterceptor {
-//
-//    @Autowired
-//    private JwtProperties jwtProperties;
-//
-//    /**
-//     * 校验jwt
-//     *
-//     * @param request
-//     * @param response
-//     * @param handler
-//     * @return
-//     * @throws Exception
-//     */
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        //判断当前拦截到的是Controller的方法还是其他资源
-//        if (!(handler instanceof HandlerMethod)) {
-//            //当前拦截到的不是动态方法，直接放行
-//            return true;
-//        }
-//
-//        //1、从请求头中获取令牌
-//        String token = request.getHeader(jwtProperties.getAdminTokenName());
-//
-//        //2、校验令牌
-//        try {
-//            log.info("jwt校验:{}", token);
-//            Claims claims = JwtUtil.parseJWT(jwtProperties.getAdminSecretKey(), token);
-//            Long empId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());
-//            log.info("当前员工id：{}", empId);
-//
-//            BaseContext.setCurrentId(empId);
-//
-//            //3、通过，放行
-//            return true;
-//        } catch (Exception ex) {
-//            //4、不通过，响应401状态码
-//            response.setStatus(401);
-//            return false;
-//        }
-//    }
-//}
+package com.example.interceptor;
+
+import com.example.constant.JwtClaimsConstant;
+import com.example.context.BaseContext;
+import com.example.properties.JwtProperties;
+import com.example.utils.JwtUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Enumeration;
+
+
+/**
+ * jwt令牌校验的拦截器
+ */
+@Component
+@Slf4j
+public class JwtTokenAdminInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    /**
+     * 校验jwt
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("jwt校验开始");
+// 假设 request 是 HttpServletRequest 类型的对象
+        Enumeration<String> headerNames = request.getHeaderNames();
+
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            System.out.println("Header Name: " + headerName);
+            // 如果还需要打印对应的值，可以用 getHeader() 方法获取
+            System.out.println("Value: " + request.getHeader(headerName));
+        }
+
+
+        //判断当前拦截到的是Controller的方法还是其他资源
+        if (!(handler instanceof HandlerMethod)) {
+            //当前拦截到的不是动态方法，直接放行
+            return true;
+        }
+
+        //1、从请求头中获取令牌
+        String token = request.getHeader(jwtProperties.getAdminTokenName());
+        log.info("请求头中的令牌：{}", token);
+        //2、校验令牌
+        try {
+            log.info("jwt校验:{}", token);
+            Claims claims = JwtUtil.parseJWT(jwtProperties.getAdminSecretKey(), token);
+            Long empId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());
+            log.info("当前员工id：{}", empId);
+
+            BaseContext.setCurrentId(empId);
+
+            //3、通过，放行
+            return true;
+        } catch (Exception ex) {
+            //4、不通过，响应401状态码
+            response.setStatus(401);
+            return false;
+        }
+    }
+}
