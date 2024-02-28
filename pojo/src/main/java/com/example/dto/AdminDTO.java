96488c664bc8d9f459fdd290fc97563869c78f5d
HouseMayer <2677296152@qq.com>
2024-02-28 13:27:28 +0800

    docs:更新了代码注释

--- pojo/src/main/java/com/example/dto/AdminDTO.java
+++ pojo/src/main/java/com/example/dto/AdminDTO.java
@@ -9,12 +9,16 @@ public class AdminDTO implements Serializable {
 
     private Long id;
 
+    //姓名
     private String name;
 
+    //用户名
     private String userName;
 
+    //电话号码
     private String phone;
 
+    //角色
     private Integer role;
 
 }
