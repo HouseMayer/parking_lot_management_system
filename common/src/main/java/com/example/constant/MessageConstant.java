51a53fe7f0561956a38703ad52455e3d18b88dd3
HouseMayer <2677296152@qq.com>
2024-02-28 23:04:30 +0800

    feat:增加区域不存在异常

--- common/src/main/java/com/example/constant/MessageConstant.java
+++ common/src/main/java/com/example/constant/MessageConstant.java
@@ -9,6 +9,8 @@ public class MessageConstant {
     public static final String ACCOUNT_NOT_FOUND = "账号不存在";
     public static final String ACCOUNT_LOCKED = "账号被锁定";
     public static final String  USERNAME_ALREADY_EXISTS = "用户名已存在";
+
+    public static final String AREA_NOT_FOUND = "区域不存在";
     public static final String  AREA_ALREADY_EXISTS = "区域名已存在";
     public static final String UNKNOWN_ERROR = "未知错误";
     public static final String ALREADT_EXISTS = "已存在";
