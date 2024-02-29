eb25a8f79baba946e9d47cef66e42e86c79b0e2c
HouseMayer <2677296152@qq.com>
2024-02-29 23:00:12 +0800

    fix:Area id注解改为自增

--- pojo/src/main/java/com/example/entity/Area.java
+++ pojo/src/main/java/com/example/entity/Area.java
@@ -26,7 +26,7 @@ public class Area implements Serializable {
     /**
      * id
      */
-    @TableId(value = "id", type = IdType.ASSIGN_ID)
+    @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
     /**
