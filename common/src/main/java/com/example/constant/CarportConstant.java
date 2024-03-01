920e8e7c2d457e41a24be52c5ee27c34d6e1ddbb
HouseMayer <2677296152@qq.com>
2024-03-01 14:51:47 +0800

    feat:车位相关常量

---
+++ common/src/main/java/com/example/constant/CarportConstant.java
@@ -0,0 +1,23 @@
+package com.example.constant;
+
+/**
+ * 状态常量，启用或者禁用
+ */
+public class CarportConstant {
+
+    //固定车位
+    public static final String FIXED_PARKING_SPACE = "固定车位";
+    //临时车位
+    public static final String TEMPORARY_PARKING_SPACE = "临时车位";
+    //露天车位
+    public static final String OPEN_PARKING_SPACE = "露天车位";
+    //室内车位
+    public static final String INDOOR_PARKING_SPACE = "室内车位";
+    //空闲
+    public static final String IDLE_STATE = "空闲";
+    //占用
+    public static final String OCCUPANCY_STATE = "占用";
+    //维修
+    public static final String MAINTENANCE_STATE = "维修";
+
+}
