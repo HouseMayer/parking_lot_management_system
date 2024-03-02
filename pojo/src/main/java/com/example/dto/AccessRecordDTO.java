b4eea063835d45cdf93de29416ccb2972df6095c
HouseMayer <2677296152@qq.com>
2024-03-02 20:34:22 +0800

    feat:新增AccessRecordDTO.java

---
+++ pojo/src/main/java/com/example/dto/AccessRecordDTO.java
@@ -0,0 +1,21 @@
+package com.example.dto;
+
+public class AccessRecordDTO {
+
+    private Long id;
+
+    //拍照
+    private String licensePlate;
+
+    //开始时间
+    private String stringTime;
+
+    //结束时间
+    private String endTime;
+
+    //车位
+    private String carport;
+
+    //费用
+    private String cost;
+}
