ef5dc018045a951861f605acdb3cf9074d0d07a2
HouseMayer <2677296152@qq.com>
2024-03-07 18:52:53 +0800

    feat:新建文件IUploadService.java，UploadServiceImpl.java

---
+++ server/src/main/java/com/example/service/impl/UploadServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.service.impl;
+
+import com.example.service.IUploadService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+
+@Slf4j
+@Service
+public class UploadServiceImpl implements IUploadService {
+
+
+
+}
