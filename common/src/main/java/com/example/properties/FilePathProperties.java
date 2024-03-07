371d48eb4200caf8382d5646f97922462b39c8e3
HouseMayer <2677296152@qq.com>
2024-03-07 21:58:53 +0800

    feat:新增FilePathProperties.java

---
+++ common/src/main/java/com/example/properties/FilePathProperties.java
@@ -0,0 +1,13 @@
+package com.example.properties;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "file")
+public class FilePathProperties {
+    String path;
+}
