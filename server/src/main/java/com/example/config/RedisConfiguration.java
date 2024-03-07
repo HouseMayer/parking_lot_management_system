0651383266b892cae9964959bfbc48ca036aff24
HouseMayer <2677296152@qq.com>
2024-03-07 21:12:09 +0800

    feat:redis相关配置

---
+++ server/src/main/java/com/example/config/RedisConfiguration.java
@@ -0,0 +1,28 @@
+package com.example.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Slf4j
+@Configuration
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        log.info("开始创建Redis模板对象...");
+        RedisTemplate redisTemplate = new RedisTemplate();
+
+        //设置Redis的连接工厂对象
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        //设置redis Key的序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+}
