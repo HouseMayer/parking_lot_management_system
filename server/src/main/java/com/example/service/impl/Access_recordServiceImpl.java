64833f395837c16d8290caf1325fe9ac4dcd8ba9
HouseMayer <2677296152@qq.com>
2024-03-08 15:12:16 +0800

    fix:出场时记录不更新的bug

--- server/src/main/java/com/example/service/impl/Access_recordServiceImpl.java
+++ server/src/main/java/com/example/service/impl/Access_recordServiceImpl.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.constant.MessageConstant;
+import com.example.context.BaseContext;
 import com.example.dto.AccessRecordDTO;
 import com.example.dto.RecordPageQueryDTO;
 import com.example.entity.AccessRecord;
@@ -19,10 +20,14 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * <p>
@@ -110,7 +115,12 @@ public class Access_recordServiceImpl extends ServiceImpl<Access_recordMapper, A
 
         // 使用BeanUtils将访问记录DTO对象的属性复制到访问记录对象中
         BeanUtils.copyProperties(accessRecordDTO, accessRecord);
-        accessRecord.setStartTime(LocalDateTime.parse(accessRecordDTO.getStartTime()));
+        if (accessRecordDTO.getStartTime() != null){
+            accessRecord.setStartTime(LocalDateTime.parse(accessRecordDTO.getStartTime()));
+        }else if (accessRecordDTO.getEndTime() != null){
+            accessRecord.setEndTime(LocalDateTime.parse(accessRecordDTO.getEndTime()));
+        }
+
         // 设置删除标记为0
         accessRecord.setDeleted(0);
         // 调用记录映射器的插入记录方法，将访问记录插入数据库
@@ -126,13 +136,16 @@ public class Access_recordServiceImpl extends ServiceImpl<Access_recordMapper, A
      */
     @Override
     public void update(AccessRecordDTO accessRecordDTO) {
+        // 根据车牌号查询对应的访问记录
+        List<AccessRecord> recordList = recordMapper.getByLicensePlate(accessRecordDTO.getLicensePlate());
 
-        // 通过ID获取访问记录
-        AccessRecord record = this.getById(accessRecordDTO.getId());
+        // 寻找开始时间最早的访问记录
+        Optional<AccessRecord> max = recordList.stream().max((Comparator.comparing(AccessRecord::getStartTime)));
+        AccessRecord record = max.get();
 
         float cost;
-        // 如果记录不存在，则直接返回
-        if (record != null){
+        // 如果未找到访问记录，则直接返回
+        if (record == null){
             return;
         }
 
@@ -142,17 +155,10 @@ public class Access_recordServiceImpl extends ServiceImpl<Access_recordMapper, A
         // 解析结束时间
         LocalDateTime endTime = LocalDateTime.parse(accessRecordDTO.getEndTime());
 
-        // 如果结束时间早于开始时间，则直接返回
-        if (endTime.isBefore(startTime)){
-            return;
-        }
-
-        // 计算访问时长
+        // 计算访问时长并根据时长计算费用
         Duration duration = Duration.between(startTime, endTime);
         Long hours = duration.toHours();
-
-        // 根据访问时长计算费用
-        if (hours <= 0){
+        if (hours < 0){
             throw new TimeException(MessageConstant.TIME_ERROR);
         }else if (hours < 2){
             cost = 0;
@@ -165,23 +171,24 @@ public class Access_recordServiceImpl extends ServiceImpl<Access_recordMapper, A
             cost = hours / 24L + (hours % 24L > 12 ? 12 : hours % 24L);
         }
 
-        // 根据车牌号查询等级
+        // 根据车牌号查询等级并根据等级调整费用
         String grade = gradeMapper.getGradeByLicensePlate(record.getLicensePlate());
-
-        // 如果车牌等级为1，费用设为0
-        if ( grade.equals("1")) {
-            cost = 0;
+        if (grade != null){
+            // 如果车牌等级为1，费用设为0
+            if ( grade.equals("1")) {
+                cost = 0;
+            }
         }
 
-        // 设置费用到DTO中
-        accessRecordDTO.setCost(String.valueOf(cost));
-
-        // 把DTO的属性值复制到访问记录对象中
-        BeanUtils.copyProperties(accessRecordDTO, record);
-
+        // 设置费用、更新时间、更新用户和结束时间
+        record.setCost(BigDecimal.valueOf(cost));
+        record.setEndTime(LocalDateTime.parse(accessRecordDTO.getEndTime()));
+        record.setUpdateTime(LocalDateTime.now());
+        record.setUpdateUser(BaseContext.getCurrentId());
         // 更新访问记录
         recordMapper.updateById(record);
     }
+
 
 
 }
