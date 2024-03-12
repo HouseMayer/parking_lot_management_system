5efd283d4db6feff0ebcc8fbeb596a6df09e74db
HouseMayer <2677296152@qq.com>
2024-03-12 19:54:36 +0800

    feat:新增ReportTaskDTO.java

---
+++ pojo/src/main/java/com/example/dto/ReportTaskDTO.java
@@ -0,0 +1,13 @@
+package com.example.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ReportTaskDTO {
+
+    private Integer total;
+
+    private BigDecimal earning;
+}
