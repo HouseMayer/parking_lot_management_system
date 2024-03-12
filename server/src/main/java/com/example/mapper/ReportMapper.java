c645a1a2539d4991c855b346a3789fbef0240344
HouseMayer <2677296152@qq.com>
2024-03-12 19:57:33 +0800

    feat:新增ReportMapper.java -新增根据日期获取总数和总收入 -新增插入report记录

--- server/src/main/java/com/example/mapper/ReportMapper.java
+++ server/src/main/java/com/example/mapper/ReportMapper.java
@@ -1,8 +1,11 @@
 package com.example.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.annotation.AutoFill;
+import com.example.dto.ReportTaskDTO;
 import com.example.entity.Report;
-import org.apache.ibatis.annotations.Mapper;
+import com.example.enumeration.OperationType;
+import org.apache.ibatis.annotations.*;
 
 /**
  * <p>
@@ -15,4 +18,16 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface ReportMapper extends BaseMapper<Report> {
 
+    @Results({
+            @Result(column = "count(*)", property = "total"),
+            @Result(column = "sum(cost)", property = "earning")
+    })
+    @Select("select count(*), sum(cost) from access_record where end_time between #{begin} and #{end}")
+    ReportTaskDTO getCountCostByDate(String begin, String end);
+
+    @AutoFill(OperationType.INSERT)
+    @Insert("insert into report(record_date, total, earning, deleted, update_time, update_user, create_time, create_user)" +
+            " values" +
+            "(#{recordDate}, #{total}, #{earning}, #{deleted}, #{updateTime}, #{updateUser}, #{createTime}, #{createUser})")
+    void insertReport(Report report);
 }
