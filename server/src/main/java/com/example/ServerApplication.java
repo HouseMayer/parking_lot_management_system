bb0b1531d5d7a201f68ac794630010a6d843d735
HouseMayer <2677296152@qq.com>
2024-03-12 19:55:22 +0800

    feat:启动类添加@EnableScheduling -启用Task

--- server/src/main/java/com/example/ServerApplication.java
+++ server/src/main/java/com/example/ServerApplication.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableTransactionManagement
 public class ServerApplication {
