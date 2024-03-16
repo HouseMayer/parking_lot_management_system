ab1c99d88a64b5200da7e710fce4d17f58f33831
HouseMayer <2677296152@qq.com>
2024-03-16 19:09:15 +0800

    feat:增加CarVO.java

---
+++ pojo/src/main/java/com/example/vo/CarVO.java
@@ -0,0 +1,13 @@
+package com.example.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CarVO {
+
+    private String picture;
+
+    private String licensePlate;
+}
