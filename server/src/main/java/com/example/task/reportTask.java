8d168e151e7bec6c48b10a198d96d8e38b7a7492
HouseMayer <2677296152@qq.com>
2024-03-17 14:19:18 +0800

    feat:增加vip黑名单到期自动删除功能

--- server/src/main/java/com/example/task/reportTask.java
+++ server/src/main/java/com/example/task/reportTask.java
@@ -1,17 +1,25 @@
 package com.example.task;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.context.BaseContext;
 import com.example.dto.ReportTaskDTO;
+import com.example.entity.Grade;
 import com.example.entity.Report;
+import com.example.mapper.GradeMapper;
 import com.example.mapper.ReportMapper;
+import com.example.service.IGradeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -19,15 +27,21 @@ public class reportTask {
 
     @Resource
     private ReportMapper reportMapper;
-
+    @Resource
+    private GradeMapper gradeMapper;
+    @Resource
+    private IGradeService gradeService;
     @Scheduled(cron = "0 0 0 * * ?")
+    @Transactional
     public void reportTask() {
         log.info("开始reportTask");
+
+
+        VIP();
+
         LocalDate yesterday = LocalDate.now().minusDays(1);
         String begin = yesterday.atStartOfDay().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         String end = yesterday.atTime(23, 59, 59).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-
-
         ReportTaskDTO countCostByDate = reportMapper.getCountCostByDate(begin, end);
         Report report = new Report();
         if (countCostByDate != null){
@@ -70,5 +84,24 @@ public class reportTask {
 
             day = day.plusDays(1);
         }
+    }
+
+
+    @Transactional
+    public void VIP() {
+
+        QueryWrapper qw = new QueryWrapper<>();
+        qw.le("deadline", LocalDate.now());
+        qw.select("id");
+        List<Integer> ids = gradeMapper.selectList(qw);
+        for (Integer id : ids) {
+            Grade grade = gradeService.getById(id);
+            grade.setUpdateTime(LocalDateTime.now());
+            grade.setUpdateUser(BaseContext.getCurrentId());
+            // 更新车辆泊位信息
+            gradeMapper.updateById(grade);
+        }
+
+        gradeMapper.deleteBatchIds(ids);
     }
 }
